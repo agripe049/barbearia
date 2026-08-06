150bae295dec3b9d590934bc29bf9b38ddabae18
agripe049 <agripeheber@gmail.com>
2026-08-06 12:30:06 -0300

    Tela do admin pronta, com testes feitos, protegendo a rota para que só possa acessar quem estiver logado. Agora falta estilizar

--- frontend/barbearia/src/pages/Admin.jsx
+++ frontend/barbearia/src/pages/Admin.jsx
@@ -20,16 +20,16 @@ const Admin = () => {
 
   const handleLogout = () => {
     removerToken();
-    navigate("/admin/login");
+    navigate("/login");
   };
 
   return (
     <div className='min-h-screen bg-gray-50 flex flex-col items-center px-4 py-10'>
       <div className='w-full max-w-md flex justify-between items-center mb-4'>
-        <h2>Painel Administrativo</h2>
+        <h2 className='text-sm font-medium text-gray-500'>Painel Administrativo</h2>
         <button
           onClick={handleLogout}
-          className='text-xs text-gray-500 hover:text-gray-900 underline'
+          className='text-xs text-gray-500 hover:text-gray-900 cursor-pointer'
         >
           Sair
         </button>
